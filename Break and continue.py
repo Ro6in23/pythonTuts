483c71db952fa26abc6826f76647a19a73292f0f
Robin <rudrack100@gmail.com>
2023-01-11 16:05:23 +0530

    initial commit

---
+++ Break and continue.py
@@ -0,0 +1,19 @@
+#i=0
+#while (True):
+ #   if i+1<5:
+  #      i=i+1
+   #     continue
+    #print(i+1, end =" ")
+    #if(i==44):
+     #   break #stop the loop
+    #i=i+1
+
+while(True):
+    print("Enter your no")
+    inp=int(input())
+    if inp>100:
+        print("Congrats")
+        break
+    else:
+        print("Try again")
+        continue
