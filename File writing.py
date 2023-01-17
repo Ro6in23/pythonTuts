e35a53138b2ec24eea4b8a64083379069ec78ca9
Robin <rudrack100@gmail.com>
2023-01-17 14:51:52 +0530

    Used File reading

---
+++ File writing.py
@@ -0,0 +1,11 @@
+f=open("Robin.txt","r")
+print(f.readline())
+print(f.readline())
+print(f.readline())
+
+# content = f.read()
+# for line in f :
+#     print(line, end=" ")
+# print(content)
+
+f.close()
---
+++ File_reading.py
@@ -0,0 +1,9 @@
+# f = open("Robin 2 .txt","w")
+# a = f.write("Robin is a great guy\n")
+# print(a)
+# f.close()
+
+# Handle read and write both
+f = open("Robin 2 .txt","r+")
+print(f.read())
+f.write("thank you")
