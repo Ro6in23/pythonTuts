9a073070b7b95afe6979d8a41dbe707c069e471f
Robin <rudrack100@gmail.com>
2023-01-17 14:51:52 +0530

    Used File reading

---
+++ File.IO basics.py
@@ -0,0 +1,11 @@
+# File IO basics
+"""
+"r" - open file for reading - default
+"w" - open file to write
+"x" - Creates files if not exist
+"a" - append - add more content to a file
+"t" - text mode - Default
+"b" - binary mode
+"+" - read and write
+"""
+
