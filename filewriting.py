44ef8fed116857bf69636a8858b6976f6167e2dd
Robin <rudrack100@gmail.com>
2023-01-25 15:12:54 +0530

    text file which will be used in other programs

---
+++ filewriting.py
@@ -0,0 +1,13 @@
+f=open("robin.txt","rt")
+# content = f.read(3)
+# print(content)
+#
+# content = f.read(3)
+# print(content)
+# for line in f:
+#     print(line,end=" ")
+#print(f.readline())
+print(f.readlines())
+
+
+f.close()
